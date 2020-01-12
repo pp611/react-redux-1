47f98b4b7ce0e5dd5e6eb6d476e1126c5b97aff8
Yi Zhao <yzhao11@yahoo.com>
2020-01-12 21:17:58 +0800

    redux thunk

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,14 +1,22 @@
 import { ADD_ARTICLE } from "../constants/action-types";
+import { DATA_LOADED } from "../constants/action-types";
 
 const initialState = {
-  articles: []
+  articles: [],
+  remoteArticles: []
 };
 
 function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
-    // state.articles.push(action.payload);
+    console.log("ADD_ARTICLE", action.payload);
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload)
+    });
+  }
+  if (action.type === DATA_LOADED) {
+    console.log("DATA_LOADED", action.payload)
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
     });
   }
   return state;
